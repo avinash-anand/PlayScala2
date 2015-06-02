ee0c1dfb756ec12da19a243b55f01790fc9e3ade
Avinash <avinash.mait@gmail.com>
2015-06-02 18:41:56 +0100

    play WS call example

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,24 @@
 package controllers
 
+import play.api.Play.current
+import play.api.libs.ws.WS
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
-  def index = Action {
+  def index1 = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def index = Action.async {
+      val call1 = WS.url("https://www.google.com").get()
+      for {
+        response <- call1
+      } yield {
+        Ok(response.body)
+      }
   }
 
 }
