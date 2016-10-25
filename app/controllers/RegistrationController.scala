881eed7ea9d51e597d950ce2300a2b63121fddfe
avinash-anand <avinash.anand@digital.hmrc.gov.uk>
2016-10-25 21:39:50 +0100

    added some more logging

--- app/controllers/RegistrationController.scala
+++ app/controllers/RegistrationController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Logger
 import play.api.mvc.{Action, Controller}
 import utils.FileUtils
 
@@ -8,18 +9,22 @@ object RegistrationController extends Controller {
   private val basePath = "conf/resources/"
 
   def registration(id: Int) = Action.async { implicit request =>
+    Logger.info(s"Inside GET Request - ID - $id")
     FileUtils.getFile(basePath + "registration" + id + ".json")
   }
 
   def submit = Action.async { implicit request =>
+    Logger.info("Inside POST request")
     FileUtils.getFile(basePath + "registration123456789.json")
   }
 
   def put = Action.async { implicit request =>
+    Logger.info("Inside PUT request")
     FileUtils.getFile(basePath + "registration_put.json")
   }
 
   def delete(id: Int) = Action.async { implicit request =>
+    Logger.info(s"Inside DELETE request - ID - $id")
     FileUtils.getFile(basePath + "registration" + id + ".json")
   }
 
